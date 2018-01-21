f9faa647adf74e3e1aed49fa4b30f40d2ca287a7
Mike Patrick <mpatrick76@gmail.com>
2018-01-21 17:28:51 -0600

    Plugin code.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-api-gateway-stage-tag-plugin
+A shim to tag API Gateway stages until CloudFormation/Serverless support arrives.
+
+Usage
+======
+
+In Serverless template:
+
+```
+plugins: 
+  - api-gateway-stage-tag-plugin
+
+custom:
+  apiStageTags:
+    TagName1: TagValue1
+    TagName2: TagValue2
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.hooks = {
+      'after:deploy:deploy': this.addTags.bind(this),
+    };
+  }
+  addTags() {
+    const stackName = `${this.serverless.service.service}-${this.options.stage}`;
+    const params = { StackName: stackName };
+
+    const awsService = this.serverless.getProvider('aws');
+    const cloudFormation = new awsService.sdk.CloudFormation({region: this.options.region});
+
+    cloudFormation.describeStackResources(params, (err, data) => {
+      if (err) {
+        this.serverless.cli.log('[ERROR]: Could not describe stack resources.');
+        this.serverless.cli.log(err);
+      } else {
+        const apiObj = data.StackResources.find((element) => {
+          return element.ResourceType === 'AWS::ApiGateway::RestApi';
+        });
+
+        const restApiId = apiObj.PhysicalResourceId;
+
+        const apigateway = new awsService.sdk.APIGateway({region: this.options.region});
+
+        const apiParams = {
+          resourceArn: `arn:aws:apigateway:${this.options.region}::/restapis/${restApiId}/stages/${this.options.stage}`,
+          tags: this.serverless.service.custom.apiStageTags
+        };
+
+        apigateway.tagResource(apiParams, (apiErr, apiData) => {
+          if (apiErr) {
+            this.serverless.cli.log('[ERROR]: Could not tag API Gateway resource');
+            this.serverless.cli.log(apiErr, apiErr.stack);
+          } else {
+            this.serverless.cli.log(`Tagged API gateway stage ${restApiId}/${this.options.stage}`);
+          }
+        });
+      }
+    });
+  }
+}
+
+module.exports = ServerlessPlugin;
